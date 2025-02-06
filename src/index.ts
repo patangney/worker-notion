4df0da0fb83843961a1cf39397e72f2c0bfd28bf
Patrick Tangney <ptangney@outlook.com>
2025-02-06 22:32:22 +0000

    setup ai bindings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+// AI bindings
+interface Bindings {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+}
+
+const app = new Hono<{ Bindings: Bindings }>(); // AI bindings that are passed to the worker
+
+// cors
+app.use(
+	'/*',
+	cors({
+		origin: '*', // Allow requests from next js app
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // Allow Content-Type to the allowed headers to fix CORS issue
+		allowMethods: ['GET', 'POST', 'OPTIONS', 'PUT'], // Allow POST method
+		exposeHeaders: ['X-Custom-Header', 'X-Kuma-Revision'], // Expose custom headers
+		maxAge: 600, // Cache preflight request for 10 minutes
+		credentials: true, // Allow credentials
+	})
+);
+
+app.post('/translateDocument', async (c) => {});
